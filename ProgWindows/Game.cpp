a856eb17d4d306d75fd5b7680f4c4d427c0460a0
Luís Mendes <106193347+luis-mendes-95@users.noreply.github.com>
2024-04-17 16:07:11 -0300

    ge engine prototype

---
+++ ProgWindows/Game.cpp
@@ -0,0 +1,33 @@
+/**********************************************************************************
+// Game (Código Fonte)
+//
+// Descrição:   Uma classe abstrata para representar um jogo.
+//
+//              Para criar um jogo o programador deve criar uma classe derivada
+//              de Game e sobrescrever os métodos Init, Update, Draw e Finalize.
+//              Para rodar o jogo, deve-se passar o objeto Game para o método
+//              Start() de um objeto Engine.
+//
+**********************************************************************************/
+
+#include "Game.h"
+#include "Engine.h"
+
+// -------------------------------------------------------------------------------
+// Inicialização de membros estáticos da classe
+
+Window*& Game::window = Engine::window;            // ponteiro para a janela
+
+// -------------------------------------------------------------------------------
+
+Game::Game()
+{
+}
+
+// -------------------------------------------------------------------------------
+
+Game::~Game()
+{
+}
+
+// -------------------------------------------------------------------------------
